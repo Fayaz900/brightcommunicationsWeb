3af965caa8c5bc6439f2538042c0c889082dce5d
Fayaz900 <fayazazeez2@gmail.com>
2026-09-13 20:19:27 +0530

    Refactor blog article image styles for consistency and clarity; streamline Next.js configuration.

--- brightcommunications-web/next.config.ts
+++ brightcommunications-web/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.1.33", "10.0.0.1"],
 };
-
-module.exports = {
-  allowedDevOrigins: ['192.168.1.33','10.0.0.1'],
-}
 
 export default nextConfig;
